808bcafcc3840bc7b68046e9296caacdfd5da036
kasara <kasara@DESKTOP-DVVB3R7>
2023-02-26 12:26:14 -0500

    Initial commit

--- src/dataTypes/DoWhile.java
+++ src/dataTypes/DoWhile.java
@@ -4,7 +4,17 @@ public class DoWhile {
 
 	public static void main(String[] args) {
 	
-
+		
+		
+		String place = "Brooklyn";
+		int start =1;
+		
+		do
+		{
+        System.out.println("I am from" +place);
+        start ++;
+         
 	}
+		
 
 }
